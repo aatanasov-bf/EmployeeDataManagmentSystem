201888c1bd3693fe6799c6922c8cc2cc93250c8b
aatanasov-bf <atanas.atanasov@blankfactor.com>
2025-01-17 10:28:58 +0200

    Add project files.

---
+++ EmployeeDataManagmentSystem/Country.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace EmployeeDataManagmentSystem
+{
+    public class Country
+    {
+        public Name Name { get; set; }
+
+        public void FetchCountryList()
+        {
+            var client = new RestClient("https://restcountries.com/v3.1/all?fields=name");
+            RestRequest request = new RestRequest();
+            RestResponse response = client.Execute(request);
+        }
+    }
+
+    public class Name
+    {
+        [JsonProperty("common")]
+        public string CountryCommonName { get; set; }
+
+        [JsonProperty("official")]
+        public string CountryOfficialName { get; set; }
+
+        [JsonProperty("nativeName")]
+        public NativeName CountryNativeName { get; set; }
+    }
+
+    public class NativeName
+    {
+        [JsonProperty("eng")]
+        public EnglishName CountryNativeName { get; set; }
+    }
+
+    public class EnglishName
+    {
+        [JsonProperty("official")]
+        public string CountryEnglishOfficialName { get; private set; }
+
+        [JsonProperty("common")]
+        public string CountryEnglishCommonName { get; private set; }
+    }
+}
---
+++ EmployeeDataManagmentSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeDataManagmentSystem;
+using Newtonsoft.Json;
+using System.Diagnostics.Metrics;
+
+Country country = new Country();
+
+country.FetchCountryList();
+
+
+
+
+
+
+
+//string json = @"
+//{
+//    ""employees"": [
+//        { ""id"": 1, ""name"": ""Alice"", ""age"": 25 },
+//        { ""id"": 2, ""name"": ""Bob"", ""age"": 30 }
+//    ]
+//}
+//";
+
+//string content = json;
+//Console.WriteLine(content);
+//File.WriteAllText("output.json", content);
